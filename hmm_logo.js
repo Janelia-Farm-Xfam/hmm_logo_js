165cb842a27958fa701eba0fc81741a5b7b2f951
neomorphic <jody@neomorphic.co.uk>
2013-07-22 15:02:34 -0400

    Column info table split into 4 columns
    The initial iteration of this table was to show all the data in one long
    column. This was not ideal for including on a web page as it took up a
    lot of vertical space and left a lot of white space on the page. So, It
    has been split into columns of 5 values or less, so that it takes up
    more horizontal space, but less vertical space.

--- hmm_logo.js
+++ hmm_logo.js
@@ -816,7 +816,9 @@
       if (options.column_info) {
         logo_graphic.bind('click', function (e) {
           var hmm_logo = logo,
-            info_tab = $('<table><tr><th>Letter</th><th>score</th></tr></table>'),
+            info_tab = $('<table class="logo_col_info"></table>'),
+            header = '<tr>',
+            tbody  = '',
             offset = $(this).offset(),
             x = parseInt((e.pageX - offset.left), 10),
 
@@ -828,11 +830,35 @@
             // clone the column data before reversal or the column gets messed
             // up in the logo when zoom levels change. Also stops flip-flopping
             // of the order from ascending to descending.
-            col_data = logo.data.height_arr[col - 1].slice(0);
-          $.each(col_data.reverse(), function (i) {
-            var values = this.split(':', 2);
-            info_tab.append('<tr><td>' + values[0] + '</td><td>' + values[1] + '</td></tr>');
-          });
+            col_data = logo.data.height_arr[col - 1].slice(0).reverse(),
+            info_cols = Math.ceil(col_data.length / 5),
+            i = 0,
+            j = 0;
+
+          //add the headers for each column.
+          for (i = 0; i < info_cols; i++) {
+            header += '<th>Residue</th><th>Score</th>';
+          }
+
+
+          header += '</tr>';
+          info_tab.append($(header));
+
+          // add the data for each column
+          for (i = 0; i < 5; i++) {
+            tbody += '<tr>';
+            j = i;
+            while (col_data[j]) {
+              var values = col_data[j].split(':', 2);
+              tbody += '<td>' + values[0] + '</td><td>' + values[1] + '</td>';
+
+              j += 5;
+            }
+            tbody += '</tr>';
+          }
+
+
+          info_tab.append($(tbody));
 
           $(options.column_info).empty().append(info_tab);
         });
